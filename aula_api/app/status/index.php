56868787835b5dc68eddb1a06e1dc25920ad8889
Rafaela Alves <100208588+ProjetRafaelaAlves1C@users.noreply.github.com>
2024-06-10 10:59:55 -0300

    Add files via upload

---
+++ aula_api/app/status/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    require_once __DIR__ . '../../../api/config.php';
+    require_once __DIR__ . '../../../api/response.php';
+
+    if(API_IS_ACTIVE){
+        echo Response::resposta(200, 'success',[
+            'version' => API_VERSION,
+            'status' => 'ATIVA'
+        ]);
+    }else {
+        echo Response::resposta(200, 'success',[
+            'version' => API_VERSION,
+            'status' => 'MANUTENÇÃO'
+        ]);
+    }
+
+?>
